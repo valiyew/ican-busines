004d8e715a8e73c12993373451239a12a3f15276
Davronali <davronali84@gmail.com>
2023-12-04 11:38:44 +0500

    test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import './assets/styles/tailwind.css'
-import { MotionPlugin } from '@vueuse/motion'
 
 import { createApp, Transition } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-app.use(MotionPlugin)
 app.component('Transition', Transition)
 
 app.use(router).mount('#app')
